34767276586b281135bf2478f3198710e2ba512e
Matt Margolin <matt.margolin@gmail.com>
2019-07-13 23:00:14 -0700

    Fix structure

--- install-spark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-sudo mkdir -p /opt/spark-2.4.0-bin-without-hadoop
-
-cd /opt; sudo wget http://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-without-hadoop.tgz
-
-sudo tar zxvf spark-2.4.0-bin-without-hadoop.tgz
-
